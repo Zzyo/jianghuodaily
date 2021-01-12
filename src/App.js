a19fd0bd80f93b9d4f837d3d9e9dc073597745ad
chenxiaojia <chenxiaojia.cxj@bytedance.com>
2021-01-12 21:44:23 +0800

    初始化

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <table>
+        <caption><h3>@江火 的2020年1月</h3></caption>
+        <tr class="head">
+            <th>星期日</th>
+            <th>星期一</th>
+            <th>星期二</th>
+            <th>星期三</th>
+            <th>星期四</th>
+            <th>星期五</th>
+            <th>星期六</th>
+        </tr>
+        <tr class="body">
+            <td class="special">27</td>
+            <td class="special">28</td>
+            <td class="special">29</td>
+            <td class="special">30</td>
+            <td class="special">31</td>
+            <td class="special">1</td>
+            <td class="special">2</td>
+        </tr>
+        <tr class="body">
+            <td class="special">3</td>
+            <td class="special">4</td>
+            <td class="special">5</td>
+            <td class="special">6</td>
+            <td class="special">7</td>
+            <td class="special">8</td>
+            <td class="special">9</td>
+        </tr>
+        <tr class="body">
+            <td class="special">10</td>
+            <td>11<ul><li><input type="checkbox"/>洗澡</li><li><input type="checkbox"/>打电话</li></ul></td>
+            <td>12<ul><li><input type="checkbox"/>洗澡</li><li><input type="checkbox"/>打电话</li></ul></td>
+            <td>13<ul><li><input type="checkbox"/>洗澡</li><li><input type="checkbox"/>打电话</li></ul></td>
+            <td>14<ul><li><input type="checkbox"/>洗澡</li><li><input type="checkbox"/>打电话</li></ul></td>
+            <td>15<ul><li><input type="checkbox"/>洗澡</li><li><input type="checkbox"/>打电话</li></ul></td>
+            <td>16<ul><li><input type="checkbox"/>洗澡</li><li><input type="checkbox"/>打电话</li><li><input type="checkbox"/>喝奶茶</li></ul></td>
+        </tr>
+        <tr class="body">
+            <td>17<ul><li><input type="checkbox"/>洗澡</li><li><input type="checkbox"/>打电话</li><li><input type="checkbox"/>喝奶茶</li></ul></td>
+            <td>18<ul><li><input type="checkbox"/>洗澡</li><li><input type="checkbox"/>打电话</li></ul></td>
+            <td>19<ul><li><input type="checkbox"/>洗澡</li><li><input type="checkbox"/>打电话</li></ul></td>
+            <td>20<ul><li><input type="checkbox"/>洗澡</li><li><input type="checkbox"/>打电话</li></ul></td>
+            <td>21<ul><li><input type="checkbox"/>洗澡</li><li><input type="checkbox"/>打电话</li></ul></td>
+            <td>22<ul><li><input type="checkbox"/>洗澡</li><li><input type="checkbox"/>打电话</li></ul></td>
+            <td>23<ul><li><input type="checkbox"/>洗澡</li><li><input type="checkbox"/>打电话</li><li><input type="checkbox"/>喝奶茶</li></ul></td>
+        </tr>
+        <tr class="body">
+            <td>24<ul><li><input type="checkbox"/>洗澡</li><li><input type="checkbox"/>打电话</li><li><input type="checkbox"/>喝奶茶</li></ul></td>
+            <td>25<ul><li><input type="checkbox"/>洗澡</li><li><input type="checkbox"/>打电话</li></ul></td>
+            <td>26<ul><li><input type="checkbox"/>洗澡</li><li><input type="checkbox"/>打电话</li></ul></td>
+            <td>27<ul><li><input type="checkbox"/>洗澡</li><li><input type="checkbox"/>打电话</li></ul></td>
+            <td>28<ul><li><input type="checkbox"/>洗澡</li><li><input type="checkbox"/>打电话</li></ul></td>
+            <td>29<ul><li><input type="checkbox"/>洗澡</li><li><input type="checkbox"/>打电话</li></ul></td>
+            <td>30<ul><li><input type="checkbox"/>洗澡</li><li><input type="checkbox"/>打电话</li><li><input type="checkbox"/>喝奶茶</li></ul></td>
+        </tr>
+        <tr class="body">
+            <td>31<ul><li><input type="checkbox"/>洗澡</li><li><input type="checkbox"/>打电话</li><li><input type="checkbox"/>喝奶茶</li></ul></td>
+        </tr>
+    </table>
+  );
+}
+
+export default App;
