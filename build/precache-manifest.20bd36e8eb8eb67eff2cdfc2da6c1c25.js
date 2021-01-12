892205b1636c08608c3c7f11314dc8b55752c147
chenxiaojia <chenxiaojia.cxj@bytedance.com>
2021-01-12 21:44:59 +0800

    初始化

---
+++ build/precache-manifest.20bd36e8eb8eb67eff2cdfc2da6c1c25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9f2c4a9dadb311af80d",
+    "url": "/static/css/main.f396437d.chunk.css"
+  },
+  {
+    "revision": "a9f2c4a9dadb311af80d",
+    "url": "/static/js/main.a9f2c4a9.chunk.js"
+  },
+  {
+    "revision": "f201d3906f8a98128054",
+    "url": "/static/js/1.f201d390.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e1ccd8baeb5da8d83dc1f89903fecb47",
+    "url": "/index.html"
+  }
+];
